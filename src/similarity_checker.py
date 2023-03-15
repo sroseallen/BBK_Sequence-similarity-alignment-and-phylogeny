517cd11a1608ae86c372ae9a2506bc1844396529
Sophie Allen (as004) <sallen10@student.bbk.ac.uk>
2023-03-15 22:41:36 +0000

    P-value and E-values across database
    A completed, working implementation of the Karlin-Altschul statistical model for defining sequence alignment significance (similar to as-used for blastn). Useful resources linked in 'Initial Notes' notebook (to be cited properly at later date).

--- src/similarity_checker.py
+++ src/similarity_checker.py
@@ -1,19 +1,14 @@
 from Bio import SeqIO
-from Bio.Seq import Seq
 from Bio import Align
-import Bio.Align.AlignInfo as AlgnInfo
 import math
 import os
 import pandas as pd
-import numpy as np
-import scipy.stats as sc
-import random
 from Bio.Phylo import TreeConstruction
 
 # To self: need to tidy up code into functions, possibly a 'Mystery' Class for the unknown sequence that allows comparison, probability table, and phylogeny functions? 
 
 # read in the dog breed database
-database_file = ".\data\dog_breeds_short.fa"
+database_file = ".\data\dog_breeds.fa"
 database_list = list(SeqIO.parse(database_file, "fasta")) #Note: parses seqeucne attributes as a string
 
 breeds = [] # initialise lists for input into pandas dataframe
@@ -53,7 +48,8 @@ aligner.mode = "local" # set to local to allow E-value calculation (model for E-
 # set up Karlin-Altschul parameters for E-value and P-value generation
 k = 0.1 # default value for k, normalising constant. Given in 'BLAST: An essential guide to the Basic Local Alignment Tool, 2003'
 m = len(mystery_seq)
-n = len("".join(dog_database["sequence"]))
+all_database_bases = "".join(dog_database["sequence"])
+n = len(all_database_bases)
 
 # frequency of all bases in the mystery sequence
 mystery_freq = {"A": mystery_seq.count("A") / m,
@@ -62,59 +58,38 @@ mystery_freq = {"A": mystery_seq.count("A") / m,
                 "G": mystery_seq.count("G") / m}
 p1 = sum(mystery_freq.values())
 
+# estimation of lambda value given frequency of bases in all of dog_database
+dog_freq = {"A": all_database_bases.count("A") / n,
+            "C": all_database_bases.count("C") / n,
+            "T": all_database_bases.count("T") / n,
+            "G": all_database_bases.count("G") / n}
+p2 = sum(dog_freq.values())
+
+# estimated score if predicted a perfect match - ie the % of bases in mystery_seq which would on average in the database align exactly like A:A, G:G, C:C or T:T
+A_A = mystery_freq["A"] * dog_freq["A"]
+C_C = mystery_freq["C"] * dog_freq["C"]
+G_G = mystery_freq["G"] * dog_freq["G"]
+T_T = mystery_freq["T"] * dog_freq["T"]
+
+average_predicted = A_A + C_C + G_G + T_T
+
+lambda_ka = math.log(1 / p1*p2) / average_predicted # equation to get Sλ, normalised score in K-A equation (converted from Sλ to λ alone by dividing by combined predicted frequency of perfectly aligned bases)
+
 # perform alignment for the mystery sequence against all sequences in the database
 for i in dog_database.index:
     print("Now checking alignment with:", i, dog_database.loc[i, "breed"]) # progress bar
     alignment = aligner.align(dog_database.loc[i, "sequence"], mystery_seq.seq) # Smith-Waterman local alignment
-    dog_database.loc[i, "align_score"] = alignment.score # writes alignment score to dataframe, saves
-    
-    # For local alignments only: Karlin-Altschul Algorithm (E- and P-value for statistical quality of alignment, same algorithm as BLASTn): Scoring Matrix Generation
-    # frequency of all bases in dog_database sequence
-    dog_freq = {"A" : 0, "C" : 0, "T" : 0, "G" : 0}
-    dog_freq = {"A": dog_database.loc[i, "sequence"].count("A") / len(dog_database.loc[i, "sequence"]),
-                "C": dog_database.loc[i, "sequence"].count("C") / len(dog_database.loc[i, "sequence"]),
-                "T": dog_database.loc[i, "sequence"].count("T") / len(dog_database.loc[i, "sequence"]),
-                "G": dog_database.loc[i, "sequence"].count("G") / len(dog_database.loc[i, "sequence"])}
-    
-    p2 = sum(dog_freq.values()) # sum of all expected frequencies, should be ~1
-
-    # count of the observed aligned pairs
-    observed_freq = {"A": {"A" : 0, "C" : 0, "T" : 0, "G" : 0}, 
-                     "C": {"A" : 0, "C" : 0, "T" : 0, "G" : 0}, 
-                     "T": {"A" : 0, "C" : 0, "T" : 0, "G" : 0}, 
-                     "G": {"A" : 0, "C" : 0, "T" : 0, "G" : 0}}
-
-    for i, j in zip(alignment.sequences[0], alignment.sequences[1]):
-        if i not in dog_freq or j not in dog_freq:
-            continue
-        else:
-            observed_freq[i][j] += 1
-
-    # converting counts into frequencies
-    q = 0 # sum of all observed frequencies, should be ~1
-    for i in observed_freq:
-        for j in observed_freq.get(i):
-            observed_freq[i][j] = observed_freq[i][j] / len(mystery_seq)
-            q += observed_freq[i][j]
-
-    normalised_score = 1 / (math.log(q / (p1*p2))) # equation to get Sλ, normalised score in K-A equation
-    print(normalised_score / alignment.score)
+    dog_database.loc[i, "raw_alignment_score"] = alignment.score # writes alignment score to dataframe, saves
 
     # generate E and P values
-    e_value = k * m * n * math.exp(-normalised_score) # probability of getting a score more than the one you generate above
+    e_value = k * m * n * math.exp(-(lambda_ka * alignment.score)) # probability of getting a score higher than the one you generate above with a random sequence
     p_value = 1 - math.exp(-e_value) # estimates p-value from e-value. Assumes a Gumbel extreme value distribution of sequences.
-    print(e_value, p_value)
-    dog_database.loc[i, "E_value"] = e_value # writes alignment score to dataframe, saves
-    dog_database.loc[i, "p_value"] = p_value # writes alignment score to dataframe, saves
+    dog_database.loc[i, "E_value"] = e_value # writes score to dataframe
+    dog_database.loc[i, "p_value"] = p_value # writes score to dataframe
 
 # output table of aligned scores sorted by alignment score
-dog_database = dog_database.sort_values("align_score")
+dog_database = dog_database.sort_values("p_value", ascending=False)
 dog_database.to_csv("results/final/similarity_alignment.csv")
-
-# probability scores
-# scipy.stats import pearsonr (standard correlation coefficient)
-# def pearsonr_pval(x,y):return pearsonr(x,y)[1]
-# corr = df.corr(method=pearsonr_pval) - generates a correlation matrix for all sequences vs all sequences in the database
 
 # phylogeny tree
 # global multiple sequence alignment for database of sequences
