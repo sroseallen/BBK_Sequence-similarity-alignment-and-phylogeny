113875b1ab9c4b6cb5459246c89cff00178d315c
Sophie Allen (as004) <sallen10@student.bbk.ac.uk>
2023-03-17 21:17:40 +0000

    Phylogeny Tree Output
    Now produces a phylogeny tree as an xml and png output (preliminary: will need tidying up at a later stage). Also adds interim outputs to the results folder for diagnostic/information purposes.

--- src/similarity_checker.py
+++ src/similarity_checker.py
@@ -1,9 +1,15 @@
 from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 from Bio import Align
+from Bio import AlignIO
+from Bio import Phylo
+from Bio.Phylo import TreeConstruction as Tree
+from Bio.Phylo.Consensus import *
 import math
 import os
 import pandas as pd
-from Bio.Phylo import TreeConstruction
+import matplotlib.pyplot as plt
 
 # To self: need to tidy up code into functions, possibly a 'Mystery' Class for the unknown sequence that allows comparison, probability table, and phylogeny functions? 
 
@@ -30,6 +36,7 @@ dog_database = pd.DataFrame (
         "align_score":0
     }
 )
+dog_database.to_csv("results/temp/dog_reference_database.csv")
 
 # read in the mystery sequence
 sequence_directory = ".\data\seq"
@@ -47,15 +54,15 @@ aligner.mode = "local" # set to local to allow E-value calculation (model for E-
 
 # set up Karlin-Altschul parameters for E-value and P-value generation
 k = 0.1 # default value for k, normalising constant. Given in 'BLAST: An essential guide to the Basic Local Alignment Tool, 2003'
-m = len(mystery_seq)
+m = len(mystery_seq.seq)
 all_database_bases = "".join(dog_database["sequence"])
 n = len(all_database_bases)
 
 # frequency of all bases in the mystery sequence
-mystery_freq = {"A": mystery_seq.count("A") / m,
-                "C": mystery_seq.count("C") / m,
-                "T": mystery_seq.count("T") / m,
-                "G": mystery_seq.count("G") / m}
+mystery_freq = {"A": mystery_seq.seq.count("A") / m,
+                "C": mystery_seq.seq.count("C") / m,
+                "T": mystery_seq.seq.count("T") / m,
+                "G": mystery_seq.seq.count("G") / m}
 p1 = sum(mystery_freq.values())
 
 # estimation of lambda value given frequency of bases in all of dog_database
@@ -77,11 +84,11 @@ lambda_ka = math.log(1 / p1*p2) / average_predicted # equation to get Sλ, norma
 
 # perform alignment for the mystery sequence against all sequences in the database
 for i in dog_database.index:
-    print("Now checking alignment with:", i, dog_database.loc[i, "breed"]) # progress bar
+    print("Now checking alignment against:", i, dog_database.loc[i, "breed"]) # progress bar
     alignment = aligner.align(dog_database.loc[i, "sequence"], mystery_seq.seq) # Smith-Waterman local alignment
     dog_database.loc[i, "raw_alignment_score"] = alignment.score # writes alignment score to dataframe, saves
 
-    # generate E and P values
+    # generate E (Expect) value and P value
     e_value = k * m * n * math.exp(-(lambda_ka * alignment.score)) # probability of getting a score higher than the one you generate above with a random sequence
     p_value = 1 - math.exp(-e_value) # estimates p-value from e-value. Assumes a Gumbel extreme value distribution of sequences.
     dog_database.loc[i, "E_value"] = e_value # writes score to dataframe
@@ -93,10 +100,31 @@ dog_database.to_csv("results/final/similarity_alignment.csv")
 
 # phylogeny tree
 # global multiple sequence alignment for database of sequences
-# calculator = DistanceCalculator('identity') - identity is the scoring model, but can check attr of calculator to see other models available
-# above.get_distance(aln) - gives a DistanceMatrix object
-# constructor = DistanceTreeConstructor(calculator, 'nj') - uses nearest neighbour joining method
-# tree = constructor.build_tree(aln)
+seqs = []
+seqs.append(SeqRecord(mystery_seq.seq, id="UNKNOWN SEQUENCE"))
+for i in dog_database.index:
+    info = (dog_database.loc[i, "sample_id"]) + (dog_database.loc[i, "breed"])
+    a = SeqRecord(Seq(dog_database.loc[i, "sequence"]), id = info)
+    seqs.append(a)
+multi_align = Align.MultipleSeqAlignment(seqs)
+AlignIO.write(multi_align, "results/temp/database_mulitpleseq_alignment.clustal", "clustal")
+
+# Distance Calculator - identity is the scoring model (default, works for both DNA and protein sequences)
+calculator = Tree.DistanceCalculator("identity")
+
 # bootstrapping to get the best tree: 
-# trees = bootstrap_trees(aln, 100, constructor, majority_consensus) - generates just the consensus tree
-# draw() to get a matplotlib rooted tree image output
+# "trees" produces 100 replicate trees, and returns just the main consensus tree (uses nearest neighbour joining method)
+boot_constructor = Tree.DistanceTreeConstructor(calculator, 'nj')
+consensus_tree = bootstrap_consensus(multi_align, 10, boot_constructor, majority_consensus)
+
+# draw() to get a matplotlib rooted tree image output (networkx not used as BioPhylo docs suggest branch lengths not indicative of evolutionary distance)
+consensus_tree.rooted = True
+consensus_tree.root.color = "green"
+
+# write the consensus tree in multiple formats
+Phylo.write(consensus_tree, "results/final/consensus_tree.xml","phyloxml")
+
+fig = plt.figure(figsize = (10, 20), dpi = 600)
+axes = fig.add_subplot(1,1,1)
+Phylo.draw(consensus_tree, axes=axes, branch_labels=lambda c: c.branch_length, do_show=False)
+plt.savefig("results/final/consensus_tree.png")
